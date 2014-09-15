6aa6d1d962afd57d8c4df692982ca19359c7032e
David N. Welton <davidw@dedasys.com>
2014-09-15 12:10:34 +0200

    Initial commit.

---
+++ src/hardcore_app.erl
@@ -0,0 +1,16 @@
+-module(hardcore_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    hardcore_sup:start_link().
+
+stop(_State) ->
+    ok.
